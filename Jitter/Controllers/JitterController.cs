8cd8b19446da2e6cc0a4632dcbe46a8325250b60
Rachel Morgan <rachyllmorgan@gmail.com>
2015-12-08 12:16:20 -0600

    styled public feed page and made connection between repo and users/jots

--- Jitter/Controllers/JitterController.cs
+++ Jitter/Controllers/JitterController.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Jitter.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace Jitter.Controllers
 {
     public class JitterController : Controller
     {
+        public JitterRepository Repo { get; set; }
+
+        public JitterController() : base()
+        {
+            Repo = new JitterRepository();
+        }
+
         // GET: Jitter
         // Maybe the Public feed here?
         public ActionResult Index()
         {
-            return View();
+            List<Jot> my_jots = Repo.GetAllJots();
+            return View(my_jots);
         }
 
         [Authorize]
@@ -24,7 +35,14 @@ namespace Jitter.Controllers
         [Authorize]
         public ActionResult UserFeed()
         {
-            return View();
+            //gets microsoft userId
+            ApplicationUserManager _userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            string userId = User.Identity.GetUserId();
+            //get application userId
+            ApplicationUser app_user = _userManager.FindById(userId);
+            JitterUser me = Repo.GetAllUsers().Where(u => u.RealUser.Id == userId).Single();
+            List<Jot> list_of_jots = Repo.GetUserJots(me);
+            return View(list_of_jots);
         }
 
         // GET: Jitter/Details/5
